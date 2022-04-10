e34275cf3f874cc02eac8f2b0f5dacb95e9e4767
Ivan Pereira <navi1921@gmail.com>
2022-04-09 21:38:01 -0300

    added extra endpoint

--- app/main.py
+++ app/main.py
@@ -1,15 +1,21 @@
 from app.classification.classifier import get_classification
-
+from app.tips import get_tip
 from fastapi import FastAPI
 
 app = FastAPI()
 
 
-@app.get("/get_classification/{text}")
+@app.get("/get_classification")
 def classification(text: str):
     tagged_words, frase_morph, tokens = get_classification(text)
     return {
         "tagged_words": tagged_words,
         "frase_morph": frase_morph,
         "tokens": tokens
+    }
+
+@app.get("/get_tip")
+def tips():
+    return {
+        "tip": get_tip()
     }
