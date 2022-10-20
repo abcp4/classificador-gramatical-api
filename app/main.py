b795f42023b0d5538084c33089b4f6d29ddd10ff
Ivan Pereira <navi1921@gmail.com>
2022-10-19 22:34:23 -0300

    add try except

--- app/main.py
+++ app/main.py
@@ -7,19 +7,33 @@ app = FastAPI()
 
 @app.get("/get_classification")
 def classification(text: str):
-#     try:
-    tagged_words, frase_morph, tokens = get_classification(text)
-    return {
-        "tagged_words": tagged_words,
-        "frase_morph": frase_morph,
-        "tokens": tokens
-    }
-#     except:
-#     return {
-#         "tagged_words": [],
-#         "frase_morph": [],
-#         "tokens": []
-#     }
+    try:
+        tagged_words, frase_morph, tokens = get_classification(text)
+        return {
+            "tagged_words": tagged_words,
+            "frase_morph": frase_morph,
+            "tokens": tokens
+        }
+     except:
+        d={
+              "tagged_words": [
+                [
+                  "erro","SUBSTANTIVO","#fd2c2c","white"
+                ],
+                " "
+              ],
+              "frase_morph": [
+                [
+                  ["Gênero","Masculino"],
+                  ["Número","Singular"
+                  ]
+                ]
+              ],
+              "tokens": [
+                "erro"
+              ]
+            }
+        return d
 
 @app.get("/get_tip")
 def tips():
