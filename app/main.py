6a9a77ba3212494d07ddbe7c767d3ea97a3662ea
Ivan Pereira <navi1921@gmail.com>
2022-10-21 18:00:50 -0300

    add logs

--- app/main.py
+++ app/main.py
@@ -1,20 +1,31 @@
 from app.classification.classifier import get_classification
 from app.tips import get_tip
 from fastapi import FastAPI
+import datetime
+
 
 app = FastAPI()
 
 
 @app.get("/get_classification")
 def classification(text: str):
+    date_str = str(datetime.date.today())
+    try:
+        o=open('/home/ubuntu/logs_classificador/'+date_str+'.txt','r')
+    except FileNotFoundError:
+        o=open('/home/ubuntu/logs_classificador/'+date_str+'.txt','w')
+    o.write(text+'\n')
     try:
         tagged_words, frase_morph, tokens = get_classification(text)
+        o.write('class: ',str(tagged_words)+'\n')
+        o.close()
         return {
             "tagged_words": tagged_words,
             "frase_morph": frase_morph,
             "tokens": tokens
         }
-    except:
+    except as e:
+        o.write('ERROR: ',str(e)+'\n')
         d={
               "tagged_words": [
                 [
@@ -33,6 +44,7 @@ def classification(text: str):
                 "erro"
               ]
             }
+        o.close()
         return d
 
 @app.get("/get_tip")
